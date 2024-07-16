4590514a5908cfd3da223860fa16aeb8b496a53b
vignanbartam <119101138+vignanbartam@users.noreply.github.com>
2024-07-16 17:37:24 +0530

    Create lecture5.java

---
+++ lecture5.java
@@ -0,0 +1,24 @@
+public class Features{
+public static void main(String[] args){
+int x = 3;
+int y = 9;
+System.out.println("X is: " +x);
+System.out.println("Y is: "+y);
+
+byte b = 10;
+System.out.println("B is: " +b);
+
+byte b2 = (byte)129;
+System.out.println("B2 is: "+b2);
+
+byte b3 = (byte)127;
+System.out.println("b3 is: "+b3);
+
+float f = (float) 3.5;
+float f2 = 5.5f;
+char ch = 'a';
+System.out.println(ch);
+int z = x+y;
+
+}
+}
